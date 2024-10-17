75a5bfe8ee5637c6730e069d3226d45b91865914
karyVane77 <kary.valencia.77@gmail.com>
2024-10-16 21:50:43 -0500

    Ejer04 -Version 4

--- src/main/java/Ejercicio4/Ejercicio2.java
+++ src/main/java/Ejercicio4/Ejercicio2.java
@@ -6,7 +6,6 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         final var arreglo = Utiles.crearArregloAleatorio(500, 10000, 100000);
         System.out.printf("El arreglo divido entre 100 es: %s%n", Arrays.toString(dividirValor(arreglo)));
-
     }
 
     private static double[] dividirValor(double[] arreglo) {
